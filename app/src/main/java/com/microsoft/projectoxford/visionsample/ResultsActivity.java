cb4eef69f592ba613b6aedcfb0ea43cb66c0a0f3
Cpzty <Cristianpzty@gmail.com>
2018-04-22 13:49:22 -0600

    Agregados los textviews y codeados los botones para modificar textviews

--- app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
@@ -14,13 +14,67 @@ public class ResultsActivity extends AppCompatActivity {
     String test;
     Float operation;
 
+    //los otros botones
+    Button btnEnergy;
+    Button btnGrasas;
+    Button btnAzucar;
+    Button btnSal;
+
+    //Textviews de todos excepto grasas saturadas
+    TextView txtEnergy;
+    TextView txtGrasas;
+    TextView txtAzucar;
+    TextView txtSal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
+        //mapeo de botones y textview de grasas saturadas
         txtPrueba = (TextView)findViewById(R.id.textViewPrueba);
         btn=(Button)findViewById(R.id.button2);
+        btnEnergy =(Button)findViewById(R.id.btnEnergia);
+        btnGrasas = (Button)findViewById(R.id.btnGrasas);
+        btnAzucar = (Button)findViewById(R.id.btnAzucar);
+        btnSal = (Button)findViewById(R.id.btnSal);
+
+        //mapeo de textviews
+        txtEnergy = (TextView)findViewById(R.id.txtEnergia);
+        txtGrasas = (TextView)findViewById(R.id.txtGrasas);
+        txtAzucar = (TextView)findViewById(R.id.txtAzucar);
+        txtSal = (TextView)findViewById(R.id.txtSal);
+
+
+        btnEnergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtEnergy.setText("BITCONNEEEEEECT");
+            }
+        });
+
+        btnGrasas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txtGrasas.setText("Funciona");
+
+            }
+        });
+
+        btnAzucar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtAzucar.setText("YEAH BOIIII");
+            }
+        });
+
+        btnSal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtSal.setText("Dota");
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
