53d6f6bc94c2be3b8fe4867af643d12db40a60d0
Unknown <Josrodjr@gmail.com>
2018-04-22 18:22:54 -0600

    Logica a los Botones

--- app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
@@ -9,22 +9,23 @@ import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    TextView txtPrueba;
-    Button btn;
-    String test;
-    Float operation;
-
     //los otros botones
     Button btnEnergy;
     Button btnGrasas;
+    Button btnSaturadas;
     Button btnAzucar;
     Button btnSal;
+
 
     //Textviews de todos excepto grasas saturadas
     TextView txtEnergy;
     TextView txtGrasas;
+    TextView txtSaturadas;
     TextView txtAzucar;
     TextView txtSal;
+
+    //valor de kJ totales que vamos a poner globales y editables mas tarde
+    float dieta = 8000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +33,38 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         //mapeo de botones y textview de grasas saturadas
-        txtPrueba = (TextView)findViewById(R.id.textViewPrueba);
-        btn=(Button)findViewById(R.id.button2);
         btnEnergy =(Button)findViewById(R.id.btnEnergia);
         btnGrasas = (Button)findViewById(R.id.btnGrasas);
+        btnSaturadas=(Button)findViewById(R.id.btnSaturadas);
         btnAzucar = (Button)findViewById(R.id.btnAzucar);
         btnSal = (Button)findViewById(R.id.btnSal);
 
         //mapeo de textviews
         txtEnergy = (TextView)findViewById(R.id.txtEnergia);
         txtGrasas = (TextView)findViewById(R.id.txtGrasas);
+        txtSaturadas = (TextView)findViewById(R.id.txtSaturadas);
         txtAzucar = (TextView)findViewById(R.id.txtAzucar);
         txtSal = (TextView)findViewById(R.id.txtSal);
 
+/*
+        String kjenergia = InputActivity.MyBundle.getString("1");
+        Float openergia = Float.parseFloat(kjenergia);
+
+        openergia = (openergia  * 100)/dieta;
+
+        txtEnergy.setText("El porcentaje de Energia de producto es: " + openergia);
+*/
 
         btnEnergy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtEnergy.setText("BITCONNEEEEEECT");
+
+               String kjenergia = InputActivity.MyBundle.getString("1");
+               Float openergia = Float.parseFloat(kjenergia);
+
+               openergia = (openergia  * 100)/dieta;
+
+               txtEnergy.setText("El porcentaje de Energia de producto es: " + openergia);
             }
         });
 
@@ -57,7 +72,23 @@ public class ResultsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                txtGrasas.setText("Funciona");
+                String grgrasas = InputActivity.MyBundle.getString("2");
+                Float kjgrasas = Float.parseFloat(grgrasas);
+                kjgrasas = (kjgrasas * 37 * 100)/dieta;
+
+                txtGrasas.setText("El porcentaje de grasas de este producto es: " + kjgrasas);
+            }
+        });
+
+        btnSaturadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String grsaturadas = InputActivity.MyBundle.getString("3");
+                Float kjsaturadas = Float.parseFloat(grsaturadas);
+                kjsaturadas = (kjsaturadas * 25 * 100)/dieta;
+
+                txtSaturadas.setText("El porcentaje de grasas saturadas de este producto es: " + kjsaturadas);
 
             }
         });
@@ -65,28 +96,28 @@ public class ResultsActivity extends AppCompatActivity {
         btnAzucar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtAzucar.setText("YEAH BOIIII");
+
+                String grazucares = InputActivity.MyBundle.getString("4");
+                Float kjazucares = Float.parseFloat(grazucares);
+                kjazucares = (kjazucares * 17 * 100)/dieta;
+
+                txtAzucar.setText("El porcentaje de azucares de este producto es: " + kjazucares);
+
             }
         });
 
         btnSal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtSal.setText("Dota");
+
+                String grsal = InputActivity.MyBundle.getString("5");
+                Float kjsal = Float.parseFloat(grsal);
+                kjsal = (kjsal * 37 * 100)/dieta;
+
+                txtSal.setText("El porcentaje de sodio de este producto es: " + kjsal);
+
             }
         });
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                test = InputActivity.MyBundle.getString("3");
-                operation = Float.parseFloat(test);
-                operation = operation*0.5f;
-                test = String.valueOf(operation);
-                txtPrueba.setText(test);
-            }
-        });
-
 
 
             }
