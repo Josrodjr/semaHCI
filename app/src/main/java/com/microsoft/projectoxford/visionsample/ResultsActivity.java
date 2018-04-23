232febf2c0340cd750bb344bbe15b4072e565727
Unknown <Josrodjr@gmail.com>
2018-04-23 10:15:42 -0600

    Pastillas funcionando

--- app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/visionsample/ResultsActivity.java
@@ -27,6 +27,47 @@ public class ResultsActivity extends AppCompatActivity {
     //valor de kJ totales que vamos a poner globales y editables mas tarde
     float dieta = 8000;
 
+    public int getCapsula(float gramos, int aplicacion){
+        int color = 0;
+        float low = 0, medium = 0, high = 0;
+
+        switch (aplicacion){
+            case 1:
+                low = 3.0f;
+                medium = 17.5f;
+                high = 17.5f;
+                break;
+            case 2:
+                low = 1.5f;
+                medium = 5.0f;
+                high = 5.0f;
+                break;
+            case 3:
+                low = 5.0f;
+                medium = 22.5f;
+                high = 22.5f;
+                break;
+            case 4:
+                low = 0.3f;
+                medium = 1.5f;
+                high = 1.5f;
+                break;
+        }
+
+        if(gramos <= low){
+            return 1;
+        }
+        if(gramos <= medium){
+            return 2;
+        }
+        if(gramos >= high){
+            return 3;
+        }
+        else{
+            return 0;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +104,64 @@ public class ResultsActivity extends AppCompatActivity {
 
         txtEnergy.setText("El porcentaje de Energia de producto es: " + openergia);
 */
+
+        String kjE = InputActivity.MyBundle.getString("1");
+        String grG = InputActivity.MyBundle.getString("2");
+        String grGS = InputActivity.MyBundle.getString("3");
+        String grA = InputActivity.MyBundle.getString("4");
+        String grS = InputActivity.MyBundle.getString("5");
+
+        Float float_kjE = Float.parseFloat(kjE);
+        Float float_grG = Float.parseFloat(grG);
+        Float float_grGS = Float.parseFloat(grGS);
+        Float float_grA = Float.parseFloat(grA);
+        Float float_grS = Float.parseFloat(grS);
+
+        int colorEtiqG = getCapsula(float_grG, 1);
+        int colorEtiqGS = getCapsula(float_grGS, 2);
+        int colorEtiqA = getCapsula(float_grA, 3);
+        int colorEtiqS = getCapsula(float_grS, 4);
+
+        if(colorEtiqG == 1){
+            btnGrasas.setBackgroundResource(R.drawable.pverde2);
+        }
+        if(colorEtiqG == 2){
+            btnGrasas.setBackgroundResource(R.drawable.pamarilla2);
+        }
+        if(colorEtiqG == 3){
+            btnGrasas.setBackgroundResource(R.drawable.proja2);
+        }
+
+
+        if(colorEtiqGS == 1){
+            btnSaturadas.setBackgroundResource(R.drawable.pverde2);
+        }
+        if(colorEtiqGS == 2){
+            btnSaturadas.setBackgroundResource(R.drawable.pamarilla2);
+        }
+        if(colorEtiqGS == 3){
+            btnSaturadas.setBackgroundResource(R.drawable.proja2);
+        }
+
+        if(colorEtiqA == 1){
+            btnAzucar.setBackgroundResource(R.drawable.pverde2);
+        }
+        if(colorEtiqA == 2){
+            btnAzucar.setBackgroundResource(R.drawable.pamarilla2);
+        }
+        if(colorEtiqA == 3){
+            btnAzucar.setBackgroundResource(R.drawable.proja2);
+        }
+
+        if(colorEtiqS == 1){
+            btnSal.setBackgroundResource(R.drawable.pverde2);
+        }
+        if(colorEtiqS == 2){
+            btnSal.setBackgroundResource(R.drawable.pamarilla2);
+        }
+        if(colorEtiqS == 3){
+            btnSal.setBackgroundResource(R.drawable.proja2);
+        }
 
         btnEnergy.setOnClickListener(new View.OnClickListener() {
             @Override
